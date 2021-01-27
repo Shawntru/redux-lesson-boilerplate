18bc1e942bb1983b8cc7a94277470540a90f6886
Shawn Truesdale <shawnctruesdale@gmail.com>
2021-01-27 15:18:39 -0700

    Connect ToDoList

---
+++ src/containers/ToDoList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const ToDoList = ({ todos }) => {
+  console.log(todos);
+
+  return <ul></ul>;
+};
+
+const mapStateToProps = (state) => ({
+  todos: state.todos,
+});
+
+export default connect(mapStateToProps)(ToDoList);
