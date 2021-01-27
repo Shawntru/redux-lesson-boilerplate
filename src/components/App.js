c0a1a62915a130af01fc7193c6b28eb63108a5d9
Shawn Truesdale <shawnctruesdale@gmail.com>
2021-01-27 15:52:48 -0700

    Move container components, link todo list

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import AddTodoForm from './AddTodoForm';
+import ToDoList from '../containers/ToDoList';
+import AddTodoForm from '../containers/AddTodoForm';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Another Todo Box</h1>
+        <h1>Todo Box</h1>
         <AddTodoForm />
+        <ToDoList />
       </div>
     );
   }
--- src/components/ToDo.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ToDo = ({ id, text, completed }) => {
-  return <li>{text}</li>;
-};
-
-export default ToDo;
